15678ddbafc0b06083e76d16f990e7dc7d3103bd
TR <thomas.rensink@gmail.com>
2019-08-11 18:48:54 -0700

    fixed "inspect" command

--- tsar/__init__.py
+++ tsar/__init__.py
@@ -9,4 +9,4 @@ REPO_PATH = os.path.split(MODULE_PATH)[0]
 METADB_PATH = os.path.join(REPO_PATH, 'tsar_dbs')
 CONTENT_FOLDER = os.path.join(REPO_PATH, 'tsar_content')
 
-_TEMP_CONTENT_FOLDER = os.path.join(REPO_PATH, '.tmp_content')
+_TEMP_CONTENT_FOLDER = os.path.join(CONTENT_FOLDER, '.tmp_content')
