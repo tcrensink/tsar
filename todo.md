2433c81c52a61715a1771d6e2efa37fb141068e6
TR <thomas.rensink@gmail.com>
2019-08-18 20:00:03 -0700

    working draft live search (no selection of results)

--- todo.md
+++ todo.md
@@ -1,5 +1,58 @@
 # V2: start with very fast, lightweight experiments on UI, UX
-# Resume test_script.py
+
+# working, bug-free
+    - tsar init: recreate metadata based on content_folder files
+    - tsar query: search current records
+    - tsar add: copy file(s) from elsewhere into content_folder, add associated metadata
+    - tsar edit: edit existing file or create new
+    - tsar inspect: create temporary db, index for exploring folder contents
+
+# prompt toolkit interface
+    X update query after each keystroke
+    - select result with up/down (maintain cursor in query)
+    - display preview of selected result
+    - open record on selection
+    - add records summary info at bottom of screen
+        - number of records
+        - available keywords (is this a cross-record type field?)
+
+
+# next: test it out, get feedback
+    - what works well?
+    - what is biggest pain point?
+    - how would you re-imagine what it offers?
+    - thoughts:
+        - improve performance
+        - provide search metadata:
+            - how many records?
+            - keywords, ranked by popularity
+        - search record previews
+        - interactive search
+        - journal function?
+        - templates?
+        - review scheduler?
+        - learn/test search syntax.  by keyword? date?
+        - improve parser
+            - keyword generation with tf-idf?
+        - load time/daemonize
+        - doc browse/association?
+    - feedback thoughts
+        - its slow
+
+
+(Glorious future/notes)
+
+# interface
+examples that may be useful, from python-prompt-toolkit/examples/full-screen/:
+- results scrolling: ./simple-demos/cursorcolumn-cursorline.py
+- in-page search: pager.py
+- change focus: focus.py
+
+
+# prompt-toolkit integration
+**presently aborted** Resume the following if a python-prompt-toolkit front end is of interest.
+The example files up through 04_autocomplete_link_to_doc.py should work out of the box, or nearly so.
+
 X install elasticsearch
 X write elasticsearch example:
     X generate document (string repr of json)
@@ -29,16 +82,44 @@ X (draft) sync index to markdown files:
     X update index id when file is edited
     X remove index id when file is removed
 
-- (draft) TUI (use prompt toolkit!)
+X (draft) TUI (use prompt toolkit!)
     X create a layout with a search bar, results widgets
     X get focus working properly:
         X typing modifies query
         X up/down selects results
 
+X fix prompt-toolkit dependency issue
+    X standard version needed for ipython, modified for tsar
+    X create forked version in `~`/git/forks
+    X recursively rename all references to `prompt_toolkit_dev`
+    X conda develop <project name!!> -n tsar
+    X verify imports work
+
+X move monkey patched example code to forked repo
+    X ElasticSearch completer
+    X fix references to prompt_toolkit_dev
+
 - (draft) define I/O for selection
     X populate preview of document for query results
     X creat "live" search/return results when updated string
-    - perform (any) pre-defined function on selection
+    - simplify: instead of buffer, use a prompt
+
+
+    - perform (any) pre-defined function when selecting a completion.  See:
+        X create a test_function that writes to file (so as not to conflict with buffer etc)
+        - what function is called when completion is *selected*?
+            - buffer.apply_completion (doesn't seem to be... and is only connected to mouse completion)
+            -
+        - buffer.
+
+        - in buffer class, see:
+               `def insert_text(self, data: str, overwrite: bool = False,
+                        move_cursor: bool = True, fire_event: bool = True) -> None:
+            """
+            Insert characters at cursor position.
+            :param fire_event: Fire `on_text_insert` event. This is mainly used to
+                trigger autocompletion while typing.
+            """`
     - open document on selection (what key?)
 
 - (draft) integrate prompt-toolkit with REPL:
---
+++ tsar/app/search_page.py
@@ -0,0 +1,256 @@
+"""query interface that executes query callback on keystrokes.
+
+
+Todo: decouple result selection from result data management
+
+RecordSelector current manages result formatting and the selected result index,
+while results_from_query is a standalone function that returns a list of result strings.
+
+Separate the functionality more clearly between result managment and selection managment as follows:
+
+class result_selector: only defines logic of selected result index
+    - attr for number of results, current result
+    - methods for changing result index
+
+class ResultManager:
+    - init:
+        - tsar_app (contains all result data from a query)
+        - result_selector (manages result selection)
+    - attr selected_index: defines current result, with help of result_selector
+    - method formatted_results: returns list of FormattedText results
+    - method `preview`: generates/returns preview of selected result
+"""
+
+from __future__ import unicode_literals
+
+import os
+from prompt_toolkit import prompt
+from prompt_toolkit.application import Application
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.layout.containers import HSplit, Window
+from prompt_toolkit.layout.controls import FormattedTextControl
+from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.layout.controls import BufferControl
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.document import Document
+from prompt_toolkit.widgets import HorizontalLine
+from functools import partial
+from prompt_toolkit.formatted_text import FormattedText
+
+STYLE = {
+    'selected': 'bg:#944288',
+    'unselected': 'default'
+}
+
+
+def return_status_text(num_results, num_records):
+    """
+    """
+    status_str =  "{num_results} of {num_records} |\
+    ".format(num_results=3, num_records=3)
+
+
+class RecordSelector(object):
+    """convert list of results to FormattedText list of tuples; index defines selected row.
+    """
+    def __init__(self, results_list=None, style=STYLE, index=None):
+
+        self.style = style
+        if results_list is None:
+            results_list = []
+        # results must always be modified in-place
+        self.results = []
+        self.update_results(
+            results_list=results_list,
+            index=index
+        )
+
+    @property
+    def result_styles(self):
+        _result_styles, _ = zip(*self.results)
+        return list(_result_styles)
+
+    @property
+    def result_texts(self):
+        _, _result_texts = zip(*self.results)
+        return list(_result_texts)
+
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self, new_index):
+        """change the results `style` based active_result_index
+        """
+        # self._index may not exist:
+        try:
+            old_index = self._index
+        except AttributeError:
+            old_index = None
+
+        # correct value bounds:
+        try:
+            if new_index >= len(self.results):
+                new_index = len(self.results) - 1
+            elif new_index < 0:
+                new_index = 0
+        except TypeError:
+            pass
+        self._index = new_index
+        self._update_selected(old_index, new_index)
+
+    def _update_selected(self, old_index, new_index):
+        """change the style of previous index to default, new index to selected
+        Either old or new index may be None and should be handled separately
+        """
+        if len(self.results) == 0:
+            return
+        try:
+            _, res = self.results[old_index]
+            self.results[old_index] = (self.style["unselected"], res)
+        except TypeError:
+            pass
+        try:
+            _, res = self.results[new_index]
+            self.results[new_index] = (self.style["selected"], res)
+        except TypeError:
+            pass
+
+    def _apply_default_format(self, results_list):
+        """convert a list of result strings to a FormattedText object
+
+        print formatted results using print_formatted_text
+        """
+        results_list = ["{}\n".format(res) for res in results_list]
+        formatted_results = FormattedText([(self.style['unselected'], res) for res in results_list])
+        return formatted_results
+
+    def update_results(self, results_list, index=None):
+        """update attributes from new results_list.  It's critical
+        that the results object maintains the same id, else they will not be updated.
+        """
+        self.results.clear()
+        self.results.extend(self._apply_default_format(results_list))
+        self.index = index
+
+
+def results_from_query(
+    query_buffer,
+    tsar_app,
+    record_selector,
+):
+    """get results from query, update formatted_results object
+    """
+    result_record_ids = tsar_app.tsar_search.query_records(
+        query_buffer.text,
+        tsar_app.tsar_db.df
+    )
+
+    results_list = []
+    rec_str = "{n} {name}, {summary}"
+    for j, record_id in enumerate(result_record_ids):
+        record = tsar_app.tsar_db.df.loc[record_id]
+        name = os.path.split(record.name)[1]
+        summary = record.file_path
+        results_list.append(rec_str.format(n=j, name=name, summary=summary))
+
+    record_selector.update_results(results_list)
+
+
+def preview_from_result_index(
+    tsar_app,
+    record_selector,
+    ):
+    """return a preview based on the current result index
+    """
+
+def run(tsar_app):
+    """LAYOUT: define buffers, assign query -> results callback"""
+
+    query_window = Window(
+        BufferControl(
+            Buffer(
+                name='query_buffer',
+                multiline=False
+            )
+        ),
+        height=1,
+    )
+
+    record_selector = RecordSelector()
+    result_window = Window(
+        FormattedTextControl(record_selector.results),
+        height=12
+    )
+
+    # callback to update results from query
+    query_window.content.buffer.on_text_changed += partial(
+        results_from_query,
+        tsar_app=tsar_app,
+        record_selector=record_selector,
+    )
+
+    preview_window = Window(
+        FormattedTextControl('(no record preview yet)'),
+        height=22
+    )
+
+    line = HorizontalLine()
+    layout = Layout(
+        HSplit([
+            Window(
+                FormattedTextControl('query:'),
+                height=1,
+                style='reverse'
+            ),
+            query_window,
+            line,
+            result_window,
+            line,
+            preview_window,
+            # Window(
+            #     BufferControl(preview_buffer),
+            # ),
+            Window(
+                FormattedTextControl('no records: {}'.format('N')),
+                height=1,
+                style='reverse'
+            ),
+        ])
+    )
+
+    ### KEYBINDINGS
+    kb = KeyBindings()
+    @kb.add('c-c')
+    def _(event):
+        " Control-c to quit application. "
+        event.app.exit()
+
+    # select result:
+    @kb.add('up')
+    @kb.add('down')
+    def _(event):
+        """change selection based on key presses
+        - update selected result (formatted_results.index)
+        - update preview
+        """
+        key = event.key_sequence[0].key
+        try:
+            if key == 'up':
+                record_selector.index -= 1
+            if key == 'down':
+                record_selector.index += 1
+        except TypeError:
+            record_selector.index = 0
+        # preview_window.content.text = return_preview(formatted_results.index)
+
+
+    ### APPLICATION
+    application = Application(
+        layout=layout,
+        key_bindings=kb,
+        full_screen=True)
+
+    application.run()
+
