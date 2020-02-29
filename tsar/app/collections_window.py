aadc86a8be593393a1ff99548b64b3da76fbff90
TR <thomas.rensink@gmail.com>
2020-02-28 19:02:07 -0800

    linting

--- tsar/app/collections_window.py
+++ tsar/app/collections_window.py
@@ -4,14 +4,14 @@ A simple example of a few buttons and click handlers.
 """
 from __future__ import unicode_literals
 from prompt_toolkit.application import Application
-from prompt_toolkit.application.current import get_app
+# from prompt_toolkit.application.current import get_app
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.key_binding.bindings.focus import (
     focus_next,
     focus_previous,
 )
-from prompt_toolkit.layout import HSplit, Layout, VSplit
-from prompt_toolkit.styles import Style
+from prompt_toolkit.layout import HSplit, Layout
+# from prompt_toolkit.styles import Style
 from prompt_toolkit.widgets import Box, Button, Frame, Label, TextArea, HorizontalLine
 from tsar.lib.collection import Collection
 import logging
