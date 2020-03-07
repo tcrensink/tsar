dd49ed2031a7f2bcf1e48adc9109a3f83f4b1be2
TR <thomas.rensink@gmail.com>
2020-03-07 13:01:47 -0800

    revised config layout

--- tsar/config.py
+++ tsar/config.py
@@ -1,23 +1,25 @@
 """TSAR config file."""
 
+# SYSTEM INTEGRATION
 EDITOR = "/usr/local/bin/subl"
-
 BROWSER = "Safari"
 ELASTICSEARCH_PORT = 9200
 
-# global keybindings
+# TSAR CONTENT DEFAULTS
+DEFAULT_COLLECTION = "arxiv"
+DEFAULT_SCREEN = "search"
+
+# GLOBAL KEY BINDINGS
 GLOBAL_KB = {
     "exit": "c-c",
-    "search_screen": ("s-right"),
-    "collections_screen": ("s-left"),
+    "search_screen": ("c-right"),
+    "collections_screen": ("c-left"),
     "open_document": "enter",
 }
 
-# color styles for results list
+# STYLING
 SEARCH_RECORD_COLORS = {
     'selected': 'bg:#144288',
     'unselected': 'default'
 }
 
-DEFAULT_COLLECTION = "lit"
-DEFAULT_SCREEN = "search"
