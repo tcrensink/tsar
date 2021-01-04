a7f7adac09d7b27bb101979fa62009166bbe1497
tcrensink <thomas.rensink@gmail.com>
2021-01-03 23:44:23 -0800

    added required dependencies

--- tsar/doctypes/__init__.py
+++ tsar/doctypes/__init__.py
@@ -1,9 +1,11 @@
 from tsar import doctypes
 from tsar.doctypes.markdown_doc import MarkdownDoc
 from tsar.doctypes.arxiv_doc import ArxivDoc
+from tsar.doctypes.youtube_doc import YoutubeDoc
 
 # list of recognized doc types
 DOCTYPES = {
     ArxivDoc.__name__: ArxivDoc,
     MarkdownDoc.__name__: MarkdownDoc,
+    YoutubeDoc.__name__: YoutubeDoc,
 }
